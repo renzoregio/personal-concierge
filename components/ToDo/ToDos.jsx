5f4e661759219dedb6005f88af852cfc223ba418
renzoregio <renzorregio@gmail.com>
2021-10-21 16:12:24 -0700

    Add Remove and Return To Do Functions

--- components/ToDo/ToDos.jsx
+++ components/ToDo/ToDos.jsx
@@ -2,7 +2,7 @@ import s from "./ToDos.module.css"
 import {useState, useRef} from "react"
 
 
-import { faCheck, faCheckCircle, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faCheckCircle, faTrash, faHistory } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
@@ -11,8 +11,10 @@ config.autoAddCss = false;
 export default function ToDos(){
 
     const [toDos, setToDos] = useState([])
+    const [completedToDos, setCompletedToDos] = useState([])
     const textBox = useRef(null)
-    const clickedToDo = useRef([])
+    const clickedPendingToDos = useRef([])
+    const clickedCompletedToDos = useRef([])
 
     const onSubmit = (e) => {
         e.preventDefault();
@@ -22,12 +24,34 @@ export default function ToDos(){
 
     const completeTask = (x) => {
         let currentToDo = x.textContent
+        setCompletedToDos([...completedToDos, currentToDo])
         const filteredToDos = toDos.filter(toDo => {
             if(toDo !== currentToDo){
                 return toDo
             }
         })
         setToDos([...filteredToDos])
+    }
+
+    const removeTask = (x) => {
+        let currentToDo = x.textContent;
+        const filteredCompletedToDos = completedToDos.filter(toDo => {
+            if(toDo !== currentToDo){
+                return toDo
+            }
+        })
+        setCompletedToDos([...filteredCompletedToDos])
+    }
+
+    const returnTask = (x) => {
+        let currentToDo = x.textContent;
+        const filteredCompletedToDos = completedToDos.filter(toDo => {
+            if(toDo !== currentToDo){
+                return toDo
+            }
+        })
+        setCompletedToDos([...filteredCompletedToDos])
+        setToDos([...toDos, currentToDo])
     }
 
     return (
@@ -42,12 +66,24 @@ export default function ToDos(){
             <div className="toDoMainContainer">
                 { toDos.map((toDo, i) => (
                     <div className={s.toDoContainer} key={i}>
-                        <span ref={refElement => clickedToDo.current[i] = refElement}  className={s.toDoText}>{toDo}</span>
-                        <FontAwesomeIcon onClick={() => completeTask(clickedToDo.current[i])} size="2x" className={s.icon} icon={faCheck}/>
-                        <FontAwesomeIcon size="2x" className={s.icon} icon={faTrash} />
+                        <span ref={refElement => clickedPendingToDos.current[i] = refElement}  className={s.toDoText}>{toDo}</span>
+                        <FontAwesomeIcon onClick={() => completeTask(clickedPendingToDos.current[i])} size="2x" className={`${s.icon} ${s.checkIcon}`} icon={faCheck}/>
                     </div>
                 ))}
             </div>
+
+            {completedToDos.length > 0 && (
+                <div> 
+                <h1 className={s.title}>completed</h1>
+                { completedToDos.map((toDo, i) => (
+                    <div className={s.toDoContainer} key={i}>
+                        <span ref={refElement => clickedCompletedToDos.current[i] = refElement} className={`${s.toDoText} ${s.completed}`}>{toDo}</span>
+                        <FontAwesomeIcon size="2x" onClick={() => removeTask(clickedCompletedToDos.current[i])} className={`${s.icon} ${s.trashIcon}`} icon={faTrash} />
+                        <FontAwesomeIcon size="2x" onClick={() => returnTask(clickedCompletedToDos.current[i])} className={`${s.icon} ${s.trashIcon}`} icon={faHistory} />
+                    </div>
+                ))}
+                </div>
+            )}
         </div>
     )
 }
