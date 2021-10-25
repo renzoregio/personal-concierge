040bc5d4f1a2fe94642107cb8b89e1a1e6332711
renzoregio <renzorregio@gmail.com>
2021-10-25 13:28:55 -0700

    Add lockNote and unlockNote functions

--- components/QuickNotes/Note.jsx
+++ components/QuickNotes/Note.jsx
@@ -1,6 +1,6 @@
 import s from "./Note.module.css"
 
-import { faPenFancy, faTrash, faTimes} from '@fortawesome/free-solid-svg-icons'
+import { faPenFancy, faTrash, faTimes, faLock, faUnlock} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
@@ -11,18 +11,25 @@ config.autoAddCss = false;
 const Note = ({title, content, deleteNote}) => {
     const [editingMode, setEditingMode] = useState(false)
     const [viewingMode, setViewingMode] = useState(false)
+    const [unlockMode, setUnlockMode] = useState(true)
     const editTextRef = useRef(content)
+
     const editNote = () => {
-        console.log(editTextRef)
         setEditingMode(true)
         editTextRef.current = content;
-        console.log(content)
+    }
 
+    const lockNote = () => {
+        setUnlockMode(false)
+    }
+
+    const unlockNote = () => {
+        setUnlockMode(true)
     }
 
     return (
     <div className={s.noteContainer}>
-            { !editingMode && <h1>{title}</h1> }
+            { !editingMode && <> <h1>{title}</h1> <FontAwesomeIcon size="2x" onClick={unlockMode ? lockNote : unlockNote} icon={unlockMode ? faUnlock : faLock} /> </> }
             <div className={!viewingMode ? s.buttonsContainer : s.viewingModeContainer}>
                 {viewingMode && 
                 <span className={s.contentTextView}>{content}</span>
@@ -32,14 +39,14 @@ const Note = ({title, content, deleteNote}) => {
                 <>
                     {!viewingMode ? 
                     <>
-                        <FontAwesomeIcon onClick={editNote} size="2x" icon={faPenFancy} />
-                        {!viewingMode && <button onClick={() => setViewingMode(true)} className={s.viewButton}>View Note</button>}
-                        <FontAwesomeIcon size="2x" onClick={() => deleteNote(title)} icon={faTrash} />
+                        { unlockMode && <FontAwesomeIcon onClick={editNote} size="2x" icon={faPenFancy} /> }
+                        {!viewingMode && unlockMode && <button onClick={() => setViewingMode(true)} className={s.viewButton}>View Note</button>}
+                        { unlockMode && <FontAwesomeIcon size="2x" onClick={() => deleteNote(title)} icon={faTrash} />}
                     </> :
                     <div style={{display: "flex", justifyContent:"space-between"}}>
                         <FontAwesomeIcon onClick={editNote} size="2x" icon={faPenFancy} />
                         <FontAwesomeIcon size="2x" onClick={() => deleteNote(title)} icon={faTrash} />
-                        <FontAwesomeIcon className={s.closeBtn} icon={faTimes} size="2x" />
+                        <FontAwesomeIcon className={s.closeBtn} onClick={() => setViewingMode(false)} icon={faTimes} size="2x" />
                     </div>
                     }
                 </> 
@@ -50,7 +57,7 @@ const Note = ({title, content, deleteNote}) => {
                         <textarea className={s.editText} defaultValue={content} ref={editTextRef}></textarea>    
                     </div>
                     <FontAwesomeIcon icon={faPenFancy} size="3x" />
-                    <FontAwesomeIcon className={s.closeBtn} icon={faTimes} size="2x" />
+                    <FontAwesomeIcon className={s.closeBtn} onClick={() => setEditingMode(false)} icon={faTimes} size="2x" />
                 </div> 
                 }
 
