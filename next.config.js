7fd4dcaa63bb59c6fbce5de6c5a47b8643b9af1b
renzoregio <renzorregio@gmail.com>
2021-11-09 17:47:10 -0800

    Update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   env: {
-    MONGO_URI: "mongodb+srv://renzoregio:dw4IibdDtfgOF5Rp@cluster0.m4geq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    MONGO_URI: "mongodb+srv://renzoregio:dw4IibdDtfgOF5Rp@cluster0.m4geq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    YELP_KEY: "",
   },
 }
